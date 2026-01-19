3deae7cef250cb16cd836f9607aef81c61c62e44
jonathanDiaz <lordgelsin26@gmail.com>
2026-01-18 21:10:47 -0500

    fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,6 +21,9 @@ const styles = {
     display: "flex",
     marginBottom: "6%",
   },
+  projectReverse: {
+    flexDirection: "row-reverse" as const,
+  },
   imageContainer: {
     flexBasis: "60%",
   },
@@ -29,13 +32,15 @@ const styles = {
     minHeight: "55vh",
     height: "auto",
   },
-
   descriptionProject: {
-    textAlign: "right",
+    textAlign: "right" as const,
     flexBasis: "40%",
     display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-evenly",
+    flexDirection: "column" as const,
+    justifyContent: "space-evenly" as const,
+  },
+  descriptionProjectLeft: {
+    textAlign: "left" as const,
   },
   projectTitle: {
     fontFamily: "Rajdhani-Bold",
@@ -46,23 +51,34 @@ const styles = {
     paddingLeft: "15%",
     backgroundColor: "#112240",
     marginLeft: "-20%",
+    position: "relative" as const,
+    zIndex: 1,
+  },
+  descriptionDetailRight: {
+    paddingLeft: "5%",
+    paddingRight: "15%",
+    marginLeft: "0",
+    marginRight: "-20%",
   },
   techUsed: {
     display: "flex",
-    flexDirection: "row",
-    listStyle: "none",
-    justifyContent: "flex-end",
+    flexDirection: "row" as const,
+    listStyle: "none" as const,
+    justifyContent: "flex-end" as const,
+  },
+  techUsedLeft: {
+    justifyContent: "flex-start" as const,
   },
   externalLinks: {
     display: "flex",
-    justifyContent: "flex-end",
-    listStyle: "none",
+    justifyContent: "flex-end" as const,
+    listStyle: "none" as const,
+  },
+  externalLinksLeft: {
+    justifyContent: "flex-start" as const,
   },
   link: {
     color: "rgb(212, 202, 202)",
-    ":hover": {
-      color: "rgb(56, 146, 182)",
-    },
   },
 } as const;
 
@@ -95,58 +111,83 @@ export default function Projects() {
         </div>
 
         <div className="container-projects">
-          {projects.map((project, index) => (
-            <div
-              key={index}
-              className={`project ${
-                index % 2 === 0 ? "project-even" : "project-odd"
-              }`}
-              style={styles.project}
-            >
-              <div style={styles.imageContainer}>
-                <Image
-                  src={project.image}
-                  alt={project.title}
-                  width={600}
-                  height={400}
-                  style={styles.image}
-                />
-              </div>
-              <div style={styles.descriptionProject}>
-                <h3 style={styles.projectTitle}>{project.title}</h3>
-                <div style={styles.descriptionDetail}>
-                  {project.description}
+          {projects.map((project, index) => {
+            const isEven = index % 2 === 0;
+
+            return (
+              <div
+                key={index}
+                className={`project ${isEven ? "project-even" : "project-odd"}`}
+                style={{
+                  ...styles.project,
+                  ...(isEven ? {} : styles.projectReverse),
+                }}
+              >
+                <div style={styles.imageContainer}>
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    width={600}
+                    height={400}
+                    style={styles.image}
+                  />
                 </div>
-                <ul style={styles.techUsed}>
-                  {project.technologies.map((tech, techIndex) => (
-                    <li
-                      style={{
-                        flexBasis: "20%",
-                        textAlign: "center",
-                      }}
-                      key={techIndex}
-                    >
-                      {tech}
+                <div
+                  style={{
+                    ...styles.descriptionProject,
+                    ...(isEven ? {} : styles.descriptionProjectLeft),
+                  }}
+                >
+                  <h3 style={styles.projectTitle}>{project.title}</h3>
+                  <div
+                    style={{
+                      ...styles.descriptionDetail,
+                      ...(isEven ? {} : styles.descriptionDetailRight),
+                    }}
+                  >
+                    {project.description}
+                  </div>
+                  <ul
+                    style={{
+                      ...styles.techUsed,
+                      ...(isEven ? {} : styles.techUsedLeft),
+                    }}
+                  >
+                    {project.technologies.map((tech, techIndex) => (
+                      <li
+                        style={{
+                          flexBasis: "20%",
+                          textAlign: "center",
+                        }}
+                        key={techIndex}
+                      >
+                        {tech}
+                      </li>
+                    ))}
+                  </ul>
+                  <ul
+                    style={{
+                      ...styles.externalLinks,
+                      ...(isEven ? {} : styles.externalLinksLeft),
+                    }}
+                  >
+                    <li style={{ margin: "10px" }}>
+                      <a
+                        href={project.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i
+                          className="fas fa-external-link-alt"
+                          style={styles.link}
+                        ></i>
+                      </a>
                     </li>
-                  ))}
-                </ul>
-                <ul style={styles.externalLinks}>
-                  <li style={{ margin: "10px" }}>
-                    <a
-                      href={project.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i
-                        className="fas fa-external-link-alt"
-                        style={styles.link}
-                      ></i>
-                    </a>
-                  </li>
-                </ul>
+                  </ul>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </section>
     </>
