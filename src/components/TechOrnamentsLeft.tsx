1db36462e2b977ec31de01031e6162d7b431a0d6
jonathanDiaz <lordgelsin26@gmail.com>
2026-02-03 11:54:43 -0500

    fix

--- src/components/TechOrnamentsLeft.tsx
+++ src/components/TechOrnamentsLeft.tsx
@@ -1,11 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function TechOrnamentsLeft() {
   return (
     <div className="ornament ornament-left">
       <ul className="tech-icons">
-        <li><i className="fab fa-aws"></i></li>
-        <li><i className="fab fa-node-js"></i></li>
-        <li><i className="fab fa-js"></i></li>
-        <li><i className="fab fa-cloudflare"></i></li>
+        <li>
+          <i className="fab fa-aws"></i>
+        </li>
+        <li>
+          <i className="fab fa-node-js"></i>
+        </li>
+        <li>
+          <i className="fab fa-js"></i>
+        </li>
+        <li>
+          <i className="fab fa-cloudflare"></i>
+        </li>
       </ul>
       <div className="vertical-ornament"></div>
     </div>
