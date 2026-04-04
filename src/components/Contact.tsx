c5aeae7a7101e330440967a48052453043961d54
jonathanDiaz <jonathan.a.diaz.m@gmail.com>
2026-04-03 19:27:29 -0500

    fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,13 +6,40 @@ export default function Contact() {
   const t = useTranslations();
 
   return (
-    <section className="contact-me">
-      <h3 className="opinion">{t("contact.messageContactMe")}</h3>
-      <h3 className="email">{t("contact.emailText")}</h3>
-      <p id="my-email">jonathan.a.diaz.m@gmail.com</p>
-      <br />
-      <h3>WhatsApp</h3>
-      <p>+51 910534676</p>
-    </section>
+    <>
+      <section className="contact-me">
+        <h3 className="opinion">{t("contact.messageContactMe")}</h3>
+        <h3 className="email">{t("contact.emailText")}</h3>
+        <p id="my-email">jonathan.a.diaz.m@gmail.com</p>
+        <br />
+        <h3>WhatsApp</h3>
+        <p>+51 910534676</p>
+      </section>
+      <style jsx>{`
+        .contact-me {
+          font-family: "Catamaran-Regular";
+          text-align: center;
+        }
+        h3,
+        p {
+          width: 47%;
+          margin: 0px auto;
+        }
+        h3.email {
+          margin-bottom: 8px;
+        }
+        p {
+          font-size: 130%;
+          text-align: center;
+        }
+
+        h3.opinion {
+          padding-bottom: 5px;
+          border-bottom: 1px solid rgb(15, 158, 158);
+          margin-bottom: 17px;
+          font-size: 170%;
+        }
+      `}</style>
+    </>
   );
 }
